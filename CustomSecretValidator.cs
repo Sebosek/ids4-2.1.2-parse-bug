ab9878b7ae717f8aa521ccd580580852d3ee8902
Sebastian Bušek <sebastian.busek@konicaminolta.cz>
2018-03-21 16:32:35 +0100

    + Log parsed secret

--- CustomSecretValidator.cs
+++ CustomSecretValidator.cs
@@ -4,14 +4,25 @@ using System.Threading.Tasks;
 
 using IdentityServer4.Models;
 using IdentityServer4.Validation;
+using Microsoft.Extensions.Logging;
 
 namespace IdS4
 {
     internal class CustomSecretValidator : ISecretValidator
     {
+        private readonly ILogger<CustomSecretValidator> _logger;
+
+        public CustomSecretValidator(ILogger<CustomSecretValidator> logger)
+        {
+            _logger = logger;
+        }
+
         public Task<SecretValidationResult> ValidateAsync(IEnumerable<Secret> secrets, ParsedSecret parsedSecret)
         {
             var given = parsedSecret.Credential as string;
+
+            _logger.LogDebug($"Parsed secret: {given}");
+
             if (secrets.Any(a => a.Value == given.Sha256()))
             {
                 return Task.FromResult(new SecretValidationResult { Success = true });
