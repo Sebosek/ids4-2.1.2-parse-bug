86c729ab220b2ad17ce54e949a1495648c4657af
Sebastian Bušek <sebastian.busek@konicaminolta.cz>
2018-03-21 16:21:03 +0100

    + Reproducable application

---
+++ Config.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+using IdentityServer4.Models;
+
+namespace IdS4
+{
+    internal class Config
+    {
+        internal static string Api => "api";
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            yield return new ApiResource(Api, "My API");
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            yield return new Client
+            {
+                ClientId = "client",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = {new Secret("1+1".Sha256())},
+                AllowedScopes = {Api}
+            };
+        }
+    }
+}
---
+++ CustomSecretValidator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using IdentityServer4.Models;
+using IdentityServer4.Validation;
+
+namespace IdS4
+{
+    internal class CustomSecretValidator : ISecretValidator
+    {
+        public Task<SecretValidationResult> ValidateAsync(IEnumerable<Secret> secrets, ParsedSecret parsedSecret)
+        {
+            var given = parsedSecret.Credential as string;
+            if (secrets.Any(a => a.Value == given.Sha256()))
+            {
+                return Task.FromResult(new SecretValidationResult { Success = true });
+            }
+
+            return Task.FromResult(new SecretValidationResult { Success = false });
+        }
+    }
+}
